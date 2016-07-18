c2192c15a14838c5319994b3c1a32777cc0237c1
Matt Hamil <j.matthew.hamil@gmail.com>
2016-07-18 14:33:27 -0500

    Added dogs to dogs array

---
+++ dogs.js
@@ -0,0 +1,26 @@
+var dogs = [
+  {
+    name: "Mr. Snugglewumps",
+    color: "Brown",
+    breed: "St. Bernard",
+    description: "A lovable dog with a great personality.",
+    price: 25,
+    url: "https://s-media-cache-ak0.pinimg.com/564x/1f/9b/8c/1f9b8c21911543f3de379a9a59584da7.jpg"
+  },
+  {
+    name: "Champ"
+    color: "Black and white"
+    breed: "Border collie"
+    description: "Shy but playful little guy!"
+    price: 30
+    url: "http://www.pets4homes.co.uk/images/breeds/1/large/41689c8d6fd802cdbd03f7f189acb863.jpg"
+  },
+  {
+    name: "Snowball",
+    color: "White",
+    breed: "Great Pyrenees",
+    description: "A big ball of floof and fun!",
+    price: 30,
+    url: "https://www.greenfieldpuppies.com/wp-content/plugins/gfp/images/big/pup_x_1366379825_0.jpg"
+  }
+];
