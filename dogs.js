07209dd3965f230ac4f69e93decb10d56589e0e2
Meg Ducharme <ducharme.meg@gmail.com>
2016-07-18 14:35:15 -0500

    Added Dogs to Array

---
+++ dogs.js
@@ -0,0 +1,28 @@
+var dogs [
+{
+  name: "Abby",
+  color: "White",
+  breed: "Miniature Poodle",
+  description: "Full of energy, smart, easy to train and loves to snuggle.",
+  price: 80,
+  URL: "https://www.google.com/search?q=miniature+poodle&espv=2&biw=1441&bih=740&source=lnms&tbm=isch&sa=X&sqi=2&ved=0ahUKEwjS9q_o3v3NAhWI6SYKHe3NC8UQ_AUIBigB#imgrc=NU45ykxKGOJZLM%3A",
+}
+
+{
+  name: "Bruce",
+  color: "Brown",
+  breed: "Bulldog",
+  description: "Playful, likes to sleep a lot and is just learning how to climb the stairs!",
+  price: 50,
+  URL: "http://cdn.earthporm.com/wp-content/uploads/2015/05/bulldog-puppy-cute-dog-photography-37__605.jpg",
+}
+
+{
+  name: "Harley",
+  color: "White and Gray",
+  breed: "Husky",
+  description: "Curious, growing quickly and loves to wrestle.",
+  price: 45,
+  URL: "https://s-media-cache-ak0.pinimg.com/564x/86/20/5d/86205db02c7f042eeecd363e6507fb6d.jpg",
+}
+
