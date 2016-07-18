4d1b6074b2d003133ad0c8dc0d9a2426d69a9278
Jacob Keen <jaclkeen@gmail.com>
2016-07-18 14:35:22 -0500

    Created an array of dog objects

---
+++ dogs.js
@@ -0,0 +1,28 @@
+var my_dogs = [
+  {
+    name: "Rex",
+    color: "Blonde",
+    breed: "Alaskan Malamute",
+    description: "Fun and energetic",
+    price: "60",
+    url: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Alaskan_Malamute.jpg"
+  },
+
+  {
+    name: "Max",
+    color: "Black",
+    breed: "Corgi",
+    description: "Happy and easy going",
+    price: "150",
+    url: "https://pbs.twimg.com/profile_images/378800000674268962/06ce58cab26c3a0daf80cf57e5acb29b_400x400.jpeg"
+  },
+
+  {
+    name: "Lucy",
+    color: "White",
+    breed: "Australian Shepard",
+    description: "Shy, but loveable",
+    price: "120",
+    url: "http://cdn2-www.dogtime.com/assets/uploads/gallery/austalian-shepherd-dog-breed-pictures/10-threequarters.jpg"
+  }
+]
