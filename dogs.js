795b8876f2209363e8406d3c66f38e19603810c2
Matt Hamil <j.matthew.hamil@gmail.com>
2016-07-18 14:39:28 -0500

    resolved conflicts between Jacob's and Matt's JS code for dogs array

---
+++ dogs.js
@@ -0,0 +1,50 @@
+var dogs = [
+  {
+    name: "Mr. Snugglewumps",
+    color: "Brown",
+    breed: "St. Bernard",
+    description: "A lovable dog with a great personality.",
+    price: 25,
+    url: "https://s-media-cache-ak0.pinimg.com/564x/1f/9b/8c/1f9b8c21911543f3de379a9a59584da7.jpg"
+  },
+  {
+    name: "Champ"
+    color: "Black and white"
+    breed: "Border collie"
+    description: "Shy but playful little guy!"
+    price: 30
+    url: "http://www.pets4homes.co.uk/images/breeds/1/large/41689c8d6fd802cdbd03f7f189acb863.jpg"
+  },
+  {
+    name: "Snowball",
+    color: "White",
+    breed: "Great Pyrenees",
+    description: "A big ball of floof and fun!",
+    price: 30,
+    url: "https://www.greenfieldpuppies.com/wp-content/plugins/gfp/images/big/pup_x_1366379825_0.jpg"
+  },
+  {
+    name: "Rex",
+    color: "Blonde",
+    breed: "Alaskan Malamute",
+    description: "Fun and energetic",
+    price: 60,
+    url: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Alaskan_Malamute.jpg"
+  },
+  {
+    name: "Max",
+    color: "Black",
+    breed: "Corgi",
+    description: "Happy and easy going",
+    price: 150,
+    url: "https://pbs.twimg.com/profile_images/378800000674268962/06ce58cab26c3a0daf80cf57e5acb29b_400x400.jpeg"
+  },
+  {
+    name: "Lucy",
+    color: "White",
+    breed: "Australian Shepard",
+    description: "Shy, but loveable",
+    price: 120,
+    url: "http://cdn2-www.dogtime.com/assets/uploads/gallery/austalian-shepherd-dog-breed-pictures/10-threequarters.jpg"
+  }
+];
